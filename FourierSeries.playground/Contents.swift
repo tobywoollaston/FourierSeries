01f33911f8b68f630c5c1355ba1306281ddb160b
Toby Woollaston <tobywoollaston@hotmail.co.uk>
2020-01-07 14:50:35 +0000

    First

---
+++ FourierSeries.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+import PlaygroundSupport
+
+let rect = CGRect(x: 0, y: 0, width: 750, height: 750)
+let view = UIView(frame: rect)
+view.backgroundColor = .white
+
+//need to use sprite kit to draw a line where the user draws
+//then use sprite kit to draw the the fourier series for each
+
+PlaygroundPage.current.liveView = view
+
